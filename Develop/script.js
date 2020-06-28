52fbf078b3f17f04aa982dfcc2d190ebc45e2398
PeeWee <oscar.oses.1127@gmail.com>
2020-06-28 10:51:59 -0400

    created prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  //Ask user how long the PW should be
+  var pwLength = prompt("How many characters would you like for your password? Choose between 8 and 128")
+  var lowerCase  = confirm("Will it contain lower case letters?")
+  var upperCase = confirm("Will it contain upper case letters?")
+  var numbers = confirm("Will it contain numbers?")
+  var specChar = confirm("Will it contain special characters?")
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
