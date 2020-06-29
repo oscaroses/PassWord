29a661ba3f48a04272b2cf74f1b1d0788b388ee0
PeeWee <oscar.oses.1127@gmail.com>
2020-06-28 21:34:21 -0400

    added questions for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "]", "{", "}", "/", ";", ":", "<", ">"]
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+character: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "]", "{", "}", "/", ";", ":", "<", ">"]
+alpha: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function generatePassword() {
   //Ask user how long the PW should be
   var pwLength = prompt("How many characters would you like for your password? Choose between 8 and 128")
-  var lowerCase  = confirm("Will it contain lower case letters?")
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Must be between 8 and 128!")
+  } else {
+
+  var lowerCase = confirm("Will it contain lower case letters?")
   var upperCase = confirm("Will it contain upper case letters?")
   var confNum = confirm("Will it contain numbers?")
   var specChar = confirm("Will it contain special characters?")
 
+  if (lowerCase === false && upperCase === false && confNum === false && specChar === false) {
+    alert("You must choose criteria!")
+  }
+  }
+
   
+
 }
 
 // Write password to the #password input
